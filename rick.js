a4972e7d5265d6b35431ea6f6d5bd7cce6eb377b
Lukas Fülling <lukas@k40s.net>
2018-04-03 17:22:16 +0200

    prevent back button
    pingpong method.

--- rick.js
+++ rick.js
@@ -1,13 +1,17 @@
 /*
 Rick.js - A script that is never gonna give you up, or let you down.
 */
-document.addEventListener('DOMContentLoaded', function() {
+(function() {
   var anchors = document.getElementsByTagName('a');
   for (var i = 0; i < anchors.length; i++) {
    anchors[i].addEventListener('click', function(e) {
      e.preventDefault();
      e.stopPropagation();
      window.location.href=atob('aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1vSGc1U0pZUkhBMA==');
+     localStorage.setItem('tematisrolodmuspimeroL','1')
    });
   }
-});
+  if(localStorage.getItem('tematisrolodmuspimeroL')) {
+    window.history.forward();
+  }
+})();
